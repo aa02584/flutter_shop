fabf9337bd1aa9f42e97a35797011ed7a11618d4
zhixinli <767628996@qq.com>
2020-01-08 17:54:56 +0800

    Initial commit

---
+++ lib/page/index_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text("首页")
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
